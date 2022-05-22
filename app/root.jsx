280dd400d6e95138c8cce526be345168d06e6da2
Chance <chancesmithb@gmail.com>
2022-05-22 16:58:46 -0500

    📦  FEAT: add first nested route

--- app/root.jsx
+++ app/root.jsx
@@ -1,29 +1,14 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-
-export const meta = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+import { LiveReload, Outlet } from "@remix-run/react";
 
 export default function App() {
   return (
     <html lang="en">
       <head>
-        <Meta />
-        <Links />
+        <meta charSet="utf-8" />
+        <title>Remix: So great, it's funny!</title>
       </head>
       <body>
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
         <LiveReload />
       </body>
     </html>
